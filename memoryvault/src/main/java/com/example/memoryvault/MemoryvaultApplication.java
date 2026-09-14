4d4ca5e9ccf914018648ffa7401632274375cb3b
yuukirintaynnyuuto <miki140518@gmail.com>
2026-09-14 16:13:12 +0900

    導入

---
+++ memoryvault/src/main/java/com/example/memoryvault/MemoryvaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.memoryvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryvaultApplication.class, args);
+	}
+
+}
